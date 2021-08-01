6cd4180dc20006673c2bc5948d27ce65d0a4059b
Prasad Kumar <prasadkumarmedicherla@gmail.com>
2021-08-01 14:05:53 +0530

    Ready

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const userRouter = require("./routes/userRouter")
 const diaryRouter = require("./routes/diaryRouter")
 
 const PORT = process.env.PORT || 5000
-const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/test"
+const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/DiaryBook"
 
 //MongoDB connection
 mongoose.connect(MONGO_URL, {
